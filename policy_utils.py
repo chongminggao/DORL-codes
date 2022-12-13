6fb7cf154f10c61e450e4023ea8b1747468a2040
Chongming GAO <chongming.gao@gmail.com>
2022-12-13 20:48:01 +0800

    kuairec还有bug

--- policy_utils.py
+++ policy_utils.py
@@ -5,7 +5,9 @@ import random
 import time
 from collections import defaultdict
 
+
 import numpy as np
+import pandas as pd
 import torch
 
 import sys
@@ -49,7 +51,7 @@ def construct_buffer_from_offline_data(args, df_train, env):
     if args.env == 'KuaiEnv-v0':
         assert hasattr(env, "lbe_user")
         df_user_num_mapped = df_user_num.loc[env.lbe_user.classes_]
-        df_user_num_mapped.reset_index(drop=True)
+        df_user_num_mapped = df_user_num_mapped.reset_index(drop=True)
         assert len(env.mat) == len(df_user_num_mapped)
     elif args.env == 'YahooEnv-v0':
         df_user_num_mapped = df_user_num.iloc[:len(env.mat)]
@@ -73,7 +75,41 @@ def construct_buffer_from_offline_data(args, df_train, env):
     # env, env_task_class, kwargs_um = get_true_env(args)
     env.max_turn = max_size
 
-    df_user_items = df_train[["user_id", "item_id", args.yfeat]].groupby("user_id").agg(list)
+    if args.env == 'KuaiEnv-v0':
+        assert hasattr(env, "lbe_item")
+        df_numpy = df_train[["user_id", "item_id", args.yfeat]].to_numpy()
+        indices = [False] * len(df_numpy)
+        for k, (user,item,yfeat) in tqdm(enumerate(df_numpy), total=len(df_numpy)):
+            if int(item) in env.lbe_item.classes_:
+                indices[k] = True
+        df_filtered = df_train[["user_id", "item_id", args.yfeat]].loc[indices]
+        df_filtered["user_id"] = 0
+        # df_user_items = df_filtered.groupby("user_id").agg(list)
+
+        num_each = np.ceil(len(df_filtered) / num_bins)
+        buffer_size = num_each * num_bins
+        buffer = VectorReplayBuffer(buffer_size, num_bins)
+
+        ind_pair = zip(np.arange(0,buffer_size,num_each), np.arange(num_each,buffer_size + num_each,num_each))
+        for left, right in ind_pair:
+
+
+
+
+
+
+
+
+
+    else:
+        # a = df_tuple.apply(lambda x: x["item_id"] in env.lbe_item.classes_ and x["user_id"] in env.lbe_user.classes_, axis=1)
+
+        df_user_items = df_train[["user_id", "item_id", args.yfeat]].groupby("user_id").agg(list)
+
+    # if hasattr(env, "lbe_user"):
+    #     df_user_items_mapped = df_user_items.loc[env.lbe_user.classes_].reset_index(drop=True)
+    # else:
+    #     df_user_items_mapped = df_user_items
     for indices, users in tqdm(bins_ind.items(), total=len(bins_ind), desc="preparing offline data into buffer..."):
         for user in users:
             items = [-1] + df_user_items.loc[user][0]
--- script/run_RL.sh
+++ script/run_RL.sh
@@ -141,17 +141,17 @@
 
 #
 #python run_Policy_BCQ.py  --read_message "pointneg" --cuda 1 --env CoatEnv-v0  --message "CoatEnv"  &
-python run_Policy_BCQ.py  --read_message "pointneg" --cuda 2 --env YahooEnv-v0 --message "YahooEnv" &
+#python run_Policy_BCQ.py  --read_message "pointneg" --cuda 2 --env YahooEnv-v0 --message "YahooEnv" &
 #python run_Policy_BCQ.py  --read_message "pointneg" --cuda 4 --env KuaiRand-v0 --message "KuaiRand" &
 python run_Policy_BCQ.py  --read_message "pointneg" --cuda 1 --env KuaiEnv-v0  --message "KuaiRand" &
 #
 #python run_Policy_CQL.py  --read_message "pointneg" --cuda 1 --env CoatEnv-v0  --message "CoatEnv"  &
-python run_Policy_CQL.py  --read_message "pointneg" --cuda 2 --env YahooEnv-v0 --message "YahooEnv" &
+#python run_Policy_CQL.py  --read_message "pointneg" --cuda 2 --env YahooEnv-v0 --message "YahooEnv" &
 #python run_Policy_CQL.py  --read_message "pointneg" --cuda 4 --env KuaiRand-v0 --message "KuaiRand" &
 python run_Policy_CQL.py  --read_message "pointneg" --cuda 1 --env KuaiEnv-v0  --message "KuaiRand" &
 #
 #python run_Policy_CRR.py  --read_message "pointneg" --cuda 4 --env CoatEnv-v0  --message "CoatEnv"  &
-python run_Policy_CRR.py  --read_message "pointneg" --cuda 1 --env YahooEnv-v0 --message "YahooEnv" &
+#python run_Policy_CRR.py  --read_message "pointneg" --cuda 1 --env YahooEnv-v0 --message "YahooEnv" &
 #python run_Policy_CRR.py  --read_message "pointneg" --cuda 6 --env KuaiRand-v0 --message "KuaiRand" &
 python run_Policy_CRR.py  --read_message "pointneg" --cuda 7 --env KuaiEnv-v0  --message "KuaiRand" &
 
@@ -167,7 +167,7 @@ python run_Policy_CRR.py  --read_message "pointneg" --cuda 7 --env KuaiEnv-v0  -
 #python run_Policy_SQN.py  --cuda 2 --which_tracker caser --which_head shead --env KuaiEnv-v0  --message "caser_shead" &
 #
 #python run_Policy_SQN.py  --cuda 1 --which_tracker caser --which_head qhead --env CoatEnv-v0  --message "caser_qhead" &
-python run_Policy_SQN.py  --cuda 2 --which_tracker caser --which_head qhead --env YahooEnv-v0 --message "caser_qhead" &
+#python run_Policy_SQN.py  --cuda 2 --which_tracker caser --which_head qhead --env YahooEnv-v0 --message "caser_qhead" &
 #python run_Policy_SQN.py  --cuda 1 --which_tracker caser --which_head qhead --env KuaiRand-v0 --message "caser_qhead" &
 python run_Policy_SQN.py  --cuda 2 --which_tracker caser --which_head qhead --env KuaiEnv-v0  --message "caser_qhead" &
 
@@ -178,13 +178,13 @@ python run_Policy_SQN.py  --cuda 2 --which_tracker caser --which_head qhead --en
 #python run_Policy_SQN.py  --cuda 5 --which_tracker gru --which_head shead --env KuaiEnv-v0  --message "gru_shead" &
 #
 #python run_Policy_SQN.py  --cuda 3 --which_tracker sasrec --which_head qhead --env CoatEnv-v0  --message "sasrec_qhead" &
-python run_Policy_SQN.py  --cuda 6 --which_tracker sasrec --which_head qhead --env YahooEnv-v0 --message "sasrec_qhead" &
+#python run_Policy_SQN.py  --cuda 6 --which_tracker sasrec --which_head qhead --env YahooEnv-v0 --message "sasrec_qhead" &
 #python run_Policy_SQN.py  --cuda 3 --which_tracker sasrec --which_head qhead --env KuaiRand-v0 --message "sasrec_qhead" &
 python run_Policy_SQN.py  --cuda 7 --which_tracker sasrec --which_head qhead --env KuaiEnv-v0  --message "sasrec_qhead" &
 #
 #
 #python run_Policy_SQN.py  --cuda 6 --which_tracker gru --which_head qhead --env CoatEnv-v0  --message "gru_qhead" &
-python run_Policy_SQN.py  --cuda 7 --which_tracker gru --which_head qhead --env YahooEnv-v0 --message "gru_qhead" &
+#python run_Policy_SQN.py  --cuda 7 --which_tracker gru --which_head qhead --env YahooEnv-v0 --message "gru_qhead" &
 #python run_Policy_SQN.py  --cuda 4 --which_tracker gru --which_head qhead --env KuaiRand-v0 --message "gru_qhead" &
 python run_Policy_SQN.py  --cuda 2 --which_tracker gru --which_head qhead --env KuaiEnv-v0  --message "gru_qhead" &
 ##
